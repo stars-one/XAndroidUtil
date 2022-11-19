2cd5b9f5789becec6f6bc0c5d4e32c29881d0c35
starone <starsone@Foxmail.com>
2022-11-19 19:30:26 +0800

    :bulb: 增加注释

--- app/src/main/java/site/starsone/xandroidutil/MainActivity.kt
+++ app/src/main/java/site/starsone/xandroidutil/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val tvHome = findViewById<RemixIconTextView>(R.id.tvHome)
+//        tvHome.iconName = "home-5-fill"
     }
 }
