f2ee2511abb35829cd2425c7cc1670838d283c56
starone <starsone@Foxmail.com>
2023-05-08 16:13:59 +0800

    :sparkles: 新增几个扩展方法

---
+++ xandroidUtil/src/main/java/site/starsone/xandroidutil/util/ExtendFun.kt
@@ -0,0 +1,68 @@
+package site.starsone.xandroidutil.util
+
+import com.google.gson.Gson
+import com.google.gson.internal.`$Gson$Types`
+import java.math.BigDecimal
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun Long.toDateString(format: String = "yyyy-MM-dd HH:mm:ss"): String {
+    return SimpleDateFormat(format).format(Date(this))
+}
+
+fun Date.toDateString(format: String = "yyyy-MM-dd HH:mm:ss"): String {
+    return SimpleDateFormat(format).format(this)
+}
+
+/**
+ * 将字节(B)转为对应的单位
+ */
+fun Long.toUnitString(): String {
+    val df = DecimalFormat("#.00")
+    val bytes = this
+    return when {
+        bytes < 1024 -> return df.format(bytes.toDouble()) + "B"
+        bytes < 1048576 -> df.format(bytes.toDouble() / 1024) + "K"
+        bytes < 1073741824 -> df.format(bytes.toDouble() / 1048576) + "Mb"
+        else -> df.format(bytes.toDouble() / 1073741824) + "Gb"
+    }
+}
+
+/**
+ * Double保留几位小数
+ *
+ * @param num
+ * @return
+ */
+fun Double.toFix(num: Int=2):Double{
+    val one = this
+    val two = BigDecimal(one)
+    return two.setScale(num, BigDecimal.ROUND_HALF_UP).toDouble()
+}
+
+/**
+ * 将json数据转为List<T>
+ *
+ * @param T 数据类型
+ * @return
+ */
+fun <T> String.parseJsonToList(clazz: Class<T>): List<T> {
+    val gson = Gson()
+    val type = `$Gson$Types`.newParameterizedTypeWithOwner(null, ArrayList::class.java, clazz)
+    val data: List<T> = gson.fromJson(this, type)
+    return data
+}
+
+/**
+ * 将json字符串数据转为某个类
+ *
+ * @param T
+ * @return
+ */
+inline fun <reified T> String.parseJsonToObject(): T {
+    val gson = Gson()
+    val result = gson.fromJson(this, T::class.java)
+    return result
+}
