9a90ee54c3dcb5cb8e44ec653cca2ea97b08ab60
starone <starsone@Foxmail.com>
2023-12-29 21:01:47 +0800

    :sparkles: showTipDialog方法增加maxHeight参数

--- xandroidUtil/src/main/java/site/starsone/xandroidutil/util/XAndroidMethod.kt
+++ xandroidUtil/src/main/java/site/starsone/xandroidutil/util/XAndroidMethod.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import com.blankj.utilcode.util.ActivityUtils
 import com.blankj.utilcode.util.SPUtils
+import com.blankj.utilcode.util.SizeUtils
 import com.kongzue.dialogx.dialogs.MessageDialog
 
 object XAndroidMethod {
@@ -38,6 +39,7 @@ object XAndroidMethod {
      * @param key 唯一标识(不再提示的标识key,存储在sp中)
      * @param message 信息内容
      * @param title 标题
+     * @param maxHeight 对话框的最大高度
      * @param okAction 点击确定后的回调
      * @param cancelAction 点击不再提示的回调
      *
@@ -46,6 +48,7 @@ object XAndroidMethod {
         key: String,
         message: String,
         title: String = "提示",
+        maxHeight: Int = 400,
         okAction: (() -> Unit)? = null,
         cancelAction: (() -> Unit)? = null
     ) {
@@ -54,6 +57,7 @@ object XAndroidMethod {
         if (instance.getBoolean(key, true)) {
 
             MessageDialog(title, message, "确定", "不再提示")
+                .setMaxHeight(SizeUtils.dp2px(maxHeight.toFloat()))
                 .setCancelButton { baseDialog, v ->
                     instance.put(key, false)
                     cancelAction?.invoke()
